8712c6ac6cd54fc0ebf4632c2a24174b5314ea26
kalashnikov <kalaexj@gmail.com>
2020-02-02 23:52:08 +0800

    OpenWeatherMap init support

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'package:clima/services/location.dart';
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -8,25 +10,43 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
-  void getLocation() async {
-
-    Position position = await Geolocator()
-        .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-    print(position);
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
   }
 
+  void getLocation() async {
+    Location loc = Location();
+    await loc.getCurrentLocation();
+    print("Pos: ${loc.latitude}, ${loc.longitude}");
+  }
+
+  void getData() async {
+    http.Response res = await http.get('http://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    if ( res.statusCode == 200 ) {
+      var codeData = jsonDecode(res.body);
+      double temp = codeData['main']['temp'];
+      int condID = codeData['weather'][0]['id'];
+      String cityName = codeData['name'];
+      print('[${condID}] ${cityName}: ${temp}');
+    } else {
+      print(res.statusCode);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
+//      body: Center(
+//        child: RaisedButton(
+//          onPressed: () {
+//            getLocation();
+//          },
+//          child: Text('Get Location'),
+//        ),
+//      ),
     );
   }
 }
---
+++ lib/services/location.dart
@@ -0,0 +1,20 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  double latitude;
+  double longitude;
+
+  Location();
+
+  Future<void> getCurrentLocation() async {
+    Position position;
+    try {
+      position = await Geolocator()
+          .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+      this.latitude = position.latitude;
+      this.longitude = position.longitude;
+    } catch(e) {
+      print(e);
+    }
+  }
+}
